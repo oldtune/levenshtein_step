32e4d751fe8ac37b236c551e2c065e89e6243c70
do.tran <do.tran@saigontechnology.com>
2022-06-17 16:03:03 +0700

    done

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
 mod matrix;
 use matrix::Matrix;
 
+use crate::matrix::find_minimum_distance;
+
 fn main() {
-    let a = String::from("honda");
-    let b = String::from("hyundai");
-    let matrix = Matrix::new(a, b);
-    matrix.print();
+    let string_a = "ha".to_string();
+    let string_b = "ha".to_string();
+
+    let distance = find_minimum_distance(&string_a, &string_b);
+    println!("{}", distance);
 }
